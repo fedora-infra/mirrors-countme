730abfca9135bc414cbff600b6399b65cd49e115
Will Woods <wwoods@redhat.com>
2020-06-19 11:00:41 -0400

    add countme-csv2sqlite.sh
    Here's a handy little script to convert the CSV output from
    countme-totals.py into a SQLite database with the same columns as the
    CSV file.
    
    The resulting database ends up being about 33% larger than than the one
    countme-totals.py makes, since it's storing two YYYY-MM-DD strings for
    'week_start' and 'week_end' instead of a nice integer 'weeknum', but I
    wanted to provide an example of how to convert CSV to SQLite.

---
+++ countme-csv2sqlite.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+    echo "usage: $0 COUNTME_CSV COUNTME_SQLITE [TABLENAME]"
+    echo "TABLENAME defaults to 'countme_totals'."
+    exit 1
+fi
+
+COUNTME_CSV="$1"
+COUNTME_SQLITE="$2"
+COUNTME_TABLE="${3:-countme_totals}"
+
+if [ ! -r "$COUNTME_CSV" ]; then
+    echo "$0: error: can't read CSV file '$COUNTME_CSV'" >&2
+    exit 2
+fi
+
+sqlite3 "$COUNTME_SQLITE" <<__SQLITE__
+.bail on
+CREATE TABLE $COUNTME_TABLE (
+    week_start DATETIME NOT NULL,
+    week_end   DATETIME NOT NULL,
+    count      INTEGER NOT NULL,
+    os_name    TEXT NOT NULL,
+    os_version TEXT NOT NULL,
+    os_variant TEXT NOT NULL,
+    os_arch    TEXT NOT NULL,
+    countme    INTEGER NOT NULL,
+    repo_tag   TEXT NOT NULL,
+    repo_arch  TEXT NOT NULL
+);
+CREATE INDEX week_start_idx ON $COUNTME_TABLE(week_start);
+.import --csv --skip 1 $COUNTME_CSV $COUNTME_TABLE
+__SQLITE__
