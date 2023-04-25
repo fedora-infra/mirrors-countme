6aac6c3db23f7ef8d23fa3b0513003f8d2ecee5d
James Antill <james@and.org>
2023-04-24 23:55:29 -0400

    Simplify Copyright line.

--- countme-delete-totals.py
+++ countme-delete-totals.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # countme-delete-totals.py - Delete the last week from the totals.db file.
 #
-# Copyright (C) 2023, Red Hat Inc.
+# Copyright Red Hat Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
