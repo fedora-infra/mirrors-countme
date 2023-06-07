afc61626696d32b6bae5673deb24b55a568a0ade
Nils Philippsen <nils@redhat.com>
2023-06-07 17:48:20 +0200

    Improve specifying time spans to trim data
    This lets users specify that data up to N weeks in the past should be
    trimmed (in fact, this is the default mode, the default time span is 13
    weeks). Trimming the oldest week is still available, but clamped by the
    set duration to avoid trimming too recent data.
    
    Additionally, it doesn’t write to the database by default, this needs
    the `--read-write` option added when running the script.
    
    Co-authored-by: James Antill <james@and.org>
    Signed-off-by: Nils Philippsen <nils@redhat.com>
    Signed-off-by: James Antill <james@and.org>

--- countme-trim-raw.py
+++ countme-trim-raw.py
@@ -23,12 +23,17 @@
 # Uses mindate() but then runs direct SQL.
 
 import argparse
+import datetime as dt
 import locale
+import math
 import sqlite3
 import time
 
 from mirrors_countme.constants import COUNTME_EPOCH, WEEK_LEN
 from mirrors_countme.version import __version__
+
+CONF_NON_RECENT_DURATION_WEEKS = 13
+WARN_SECONDS = 5
 
 locale.setlocale(locale.LC_ALL, "")
 
@@ -37,99 +42,141 @@ locale.setlocale(locale.LC_ALL, "")
 # ===========================================================================
 
 
+def positive_int(value):
+    value = int(value)
+    if value < 1:
+        raise ValueError("Value must be > 1")
+    return value
+
+
 def parse_args(argv=None):
     p = argparse.ArgumentParser(
-        description="Parse Fedora access.log files.",
+        description="Trim data from raw.db files.",
     )
     p.add_argument("-V", "--version", action="version", version=f"%(prog)s {__version__}")
 
     p.add_argument(
-        "--sqlite",
+        "sqlite",
         metavar="DBFILE",
-        help="write to a sqlite database",
+        help="Write to a sqlite database.",
+    )
+    p.add_argument(
+        "--rw",
+        "--read-write",
+        dest="rw",
+        default=False,
+        action="store_true",
+        help="Actually delete the entries.",
     )
     p.add_argument(
         "--noop",
         dest="rw",
-        default=True,
+        default=False,
         action="store_false",
-        help="Skip deleting the entries.",
+        help="Don’t actually delete the entries. (default)",
+    )
+
+    p.add_argument(
+        "--oldest-week", action="store_true", help="Trim the oldest (full) week of data."
+    )
+    p.add_argument(
+        "keep",
+        metavar="KEEP",
+        type=int,
+        nargs="?",
+        default=CONF_NON_RECENT_DURATION_WEEKS,
+        help=(
+            f"Only trim data older than KEEP weeks ago. (default: {CONF_NON_RECENT_DURATION_WEEKS})"
+        ),
     )
 
     args = p.parse_args(argv)
 
-    return args
+    return p, args
 
 
 # Mostly borrowed from mirrors_countme/__init__
-def get_mintime(connection):
+def get_mintime(connection: sqlite3.Connection):
     cursor = connection.execute("SELECT MIN(timestamp) FROM countme_raw")
     return cursor.fetchone()[0]
 
 
+def get_maxtime(connection: sqlite3.Connection):
+    cursor = connection.execute("SELECT MAX(timestamp) FROM countme_raw")
+    return cursor.fetchone()[0]
+
+
 # Find the next week to trim, given the earliest timestamp.
-def next_week(mintime):
-    begin = COUNTME_EPOCH
-    while begin <= mintime:
-        begin += WEEK_LEN
-    # Now begin is the first week _after_ the mintime.
-    return begin
+def next_week(mintime: int | float) -> int:
+    week_num = math.ceil((mintime - COUNTME_EPOCH) / WEEK_LEN)
+    return COUNTME_EPOCH + week_num * WEEK_LEN
 
 
-def _num_entries_before(connection, timestamp):
+def _num_entries(connection: sqlite3.Connection, trim_begin: int | float, trim_end: int | float):
     cursor = connection.execute(
-        "SELECT COUNT(*) FROM countme_raw WHERE timestamp < ?", (timestamp,)
+        "SELECT COUNT(*) FROM countme_raw WHERE timestamp >= ? AND timestamp < ?",
+        (trim_begin, trim_end),
     )
     return cursor.fetchone()[0]
 
 
-def _num_entries(connection):
-    cursor = connection.execute("SELECT COUNT(*) FROM countme_raw")
-    return cursor.fetchone()[0]
-
-
-def _del_entries_before(connection, timestamp):
-    connection.execute("DELETE FROM countme_raw WHERE timestamp < ?", (timestamp,))
+def _del_entries(connection: sqlite3.Connection, trim_begin: int | float, trim_end: int | float):
+    connection.execute(
+        "DELETE FROM countme_raw WHERE timestamp >= ? AND timestamp < ?", (trim_begin, trim_end)
+    )
     connection.commit()
 
 
 def tm2ui(timestamp):
-    tm = time.gmtime(timestamp)
-    return time.strftime("%Y-%m-%d %H:%M:%S", tm)
+    date = dt.datetime.fromtimestamp(timestamp, tz=dt.UTC).date()
+    return date.isoformat()
 
 
-def num2ui(num):
-    ret = locale.format_string("%d", num, grouping=True)
-    mlen = len("100,000,000")
-    if len(ret) < mlen:
-        ret = " " * (mlen - len(ret)) + ret
-    return ret
+def trim_data(
+    *, connection: sqlite3.Connection, trim_begin: int | float, trim_end: int | float, rw: bool
+):
+    num_affected = _num_entries(connection, trim_begin, trim_end)
+    if rw:
+        print(f" ** About to DELETE data from {tm2ui(trim_begin)} to {tm2ui(trim_end)}. **")
+        print(f" ** This will affect {num_affected} entries. **")
+        print(f" ** Interrupt within {WARN_SECONDS} seconds to prevent that. **")
+        time.sleep(WARN_SECONDS)
+        print(" ** DELETING data … **")
+        _del_entries(connection, trim_begin, trim_end)
+        print(" ** Done. **")
+    else:
+        print(f" ** Not deleting data from {tm2ui(trim_begin)} to {tm2ui(trim_end)}. **")
+        print(f" ** This would affect {num_affected} entries. **")
 
 
-def get_trim_data(sqlite_filename):
-    connection = sqlite3.connect(f"file:{sqlite_filename}?mode=rwc", uri=True)
-    mintime = get_mintime(connection)
-    week = next_week(mintime)
+# Real main...
+def _main():
+    parser, args = parse_args()
 
-    print("First timestamp:", tm2ui(mintime))
-    print("Next week      :", tm2ui(week))
-    print("Entries        :", num2ui(_num_entries(connection)))
-    print("Entries to trim:", num2ui(_num_entries_before(connection, week)))
+    sqlite_uri = f"file:{args.sqlite}?mode=rwc"
+    connection = sqlite3.connect(sqlite_uri, uri=True)
 
-    return connection, week
+    # Find out what timespan is covered by data in database.
+    min_time = get_mintime(connection)
+    max_time = get_maxtime(connection)
 
+    # Calculate last monday midnight UTC in data, to be used for calculating whole week boundaries.
+    last = dt.datetime.fromtimestamp(max_time, tz=dt.UTC)
+    last_midnight = last.replace(hour=0, minute=0, second=0, microsecond=0)
+    last_monday_midnight = last_midnight - dt.timedelta(days=last_midnight.weekday())
+    # Add 1 week because the last week contained likely isn’t complete.
+    cutoff_monday_midnight = last_monday_midnight - dt.timedelta(weeks=args.keep + 1)
 
-def trim_data(connection, week):
-    print(" ** About to DELETE data. **")
-    time.sleep(5)
-    _del_entries_before(connection, week)
+    trim_begin = min_time
+    trim_end = max(cutoff_monday_midnight.timestamp(), trim_begin)
+    if args.oldest_week:
+        trim_end = min(next_week(trim_begin), trim_end)
+
+    trim_data(connection=connection, trim_begin=trim_begin, trim_end=trim_end, rw=args.rw)
 
 
 if __name__ == "__main__":
     try:
-        args = parse_args()
-        connection, week = get_trim_data(args.sqlite)
-        if args.rw:
-            trim_data(connection, week)
+        _main()
     except KeyboardInterrupt:
         raise SystemExit(3)  # sure, 3 is good, why not
