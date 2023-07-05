ac897b2da2c03e912a27eacd0c498de581f3bc07
Nils Philippsen <nils@redhat.com>
2023-07-05 11:10:04 +0200

    Completely cover util module in its unit tests
    And fix a typo.
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,10 +1,10 @@
-import datetime
+import datetime as dt
 from contextlib import nullcontext
 
 import pytest
 
+from mirrors_countme import util
 from mirrors_countme.constants import COUNTME_EPOCH
-from mirrors_countme.util import offset_to_timezone, weeknum
 
 
 @pytest.mark.parametrize(
@@ -22,7 +22,7 @@ def test_weeknum(timestamp, expected):
         expectation = pytest.raises(expected)
 
     with expectation:
-        obtained = weeknum(timestamp)
+        obtained = util.weeknum(timestamp)
 
     if isinstance(expected, int):
         assert obtained == expected
@@ -31,19 +31,49 @@ def test_weeknum(timestamp, expected):
 @pytest.mark.parametrize(
     "offset, expected",
     [
-        ("-0400", datetime.timezone(datetime.timedelta(days=-1, seconds=72000))),
-        ("+0500", datetime.timezone(datetime.timedelta(seconds=18000))),
+        ("-0400", dt.timezone(dt.timedelta(days=-1, seconds=72000))),
+        ("+0500", dt.timezone(dt.timedelta(seconds=18000))),
         ("1683208046.7402434", ValueError),
     ],
 )
-def test_offest_to_timezone(offset, expected):
-    if isinstance(expected, datetime.timezone):
+def test_offset_to_timezone(offset, expected):
+    if isinstance(expected, dt.timezone):
         expectation = nullcontext()
     else:
         expectation = pytest.raises(expected)
 
     with expectation:
-        obtained = offset_to_timezone(offset)
+        obtained = util.offset_to_timezone(offset)
 
-    if isinstance(expected, datetime.timezone):
+    if isinstance(expected, dt.timezone):
         assert obtained == expected
+
+
+@pytest.mark.parametrize(
+    "logtime, expected",
+    (
+        ("29/Mar/2020:16:04:28 +0000", dt.datetime(2020, 3, 29, 16, 4, 28, 0, dt.UTC)),
+        ("29/Mar/2020:16:04:28 -0000", dt.datetime(2020, 3, 29, 16, 4, 28, 0, dt.UTC)),
+        (
+            "29/Mar/2020:16:04:28 +0200",
+            dt.datetime(2020, 3, 29, 16, 4, 28, 0, dt.timezone(dt.timedelta(hours=2))),
+        ),
+        (
+            "29/Mar/2020:16:04:28 -0800",
+            dt.datetime(2020, 3, 29, 16, 4, 28, 0, dt.timezone(dt.timedelta(hours=-8))),
+        ),
+    ),
+)
+def test_parse_logtime(logtime, expected):
+    assert util.parse_logtime(logtime) == expected
+
+
+@pytest.mark.parametrize(
+    "querystr, expected",
+    (
+        ("foo=bar&baz=gnu", {"foo": "bar", "baz": "gnu"}),
+        ("foo=bar&foo=baz", {"foo": "baz"}),
+    ),
+)
+def test_parse_querydict(querystr, expected):
+    assert util.parse_querydict(querystr) == expected
